2ff54abe0bc99e9a9992f84798317310f66e0ef6
bgraknd <bgraknd@gmail.com>
2019-02-15 11:16:16 +0300

    add customize ArrayAdapter

--- app/src/main/java/com/example/bugra/burclar/MainActivity.kt
+++ app/src/main/java/com/example/bugra/burclar/MainActivity.kt
@@ -2,18 +2,18 @@ package com.example.bugra.burclar
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.widget.ArrayAdapter
-import android.widget.TextView
-import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    var burclar = arrayOf("Koç", "Boğa", "İkizler", "Yengeç", "Aslan", "Başak", "Terazi", "Akrep", "Yay", "Oğlak", "Kova", "Balık")
+    //var burclar = arrayOf("Koç", "Boğa", "İkizler", "Yengeç", "Aslan", "Başak", "Terazi", "Akrep", "Yay", "Oğlak", "Kova", "Balık")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /* Bu Alttaki kisimlarda arrayadapter yeterli gelmedigi icin iptal edildi
+
 
         //var burclarAdapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, burclar)
         var burclarAdapter = ArrayAdapter<String>(this, R.layout.tek_satir, R.id.tvBurcAdi, burclar)
@@ -31,5 +31,21 @@ class MainActivity : AppCompatActivity() {
             var gecici = view as TextView
             Toast.makeText(this@MainActivity, "Tiklandi: "+gecici.text.toString()+ " Pos: "+i, Toast.LENGTH_SHORT).show()
         }
+        */
+
+        var burclar = resources.getStringArray(R.array.burclar)
+        var burcTarihleri = resources.getStringArray(R.array.burcTarih)
+        var burcResimleri = arrayOf(
+            R.drawable.aries1, R.drawable.taurus2, R.drawable.gemini3, R.drawable.cancer4,
+            R.drawable.leo5, R.drawable.virgo6, R.drawable.libra7, R.drawable.scorpio8, R.drawable.sagittarius9,
+            R.drawable.capricorn10, R.drawable.aquarius11, R.drawable.pisces12
+        )
+
+        //var myAdapter = ArrayAdapter<String>(this, R.layout.tek_satir, R.id.tvBurcAdi, burclar)
+        var myAdapter =
+            BurclarArrayAdaptor(this, R.layout.tek_satir, R.id.tvBurcAdi, burclar, burcTarihleri, burcResimleri)
+        listBurclar.adapter = myAdapter
+
+
     }
 }
